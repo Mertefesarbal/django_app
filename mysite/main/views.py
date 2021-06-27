d3400e250ddfc25d3495bb655971bd6c4f0ff948
Ozan <cetino19@itu.edu.tr>
2021-06-27 15:12:55 +0300

    views refreshed

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-# Create your views here.
+# TemplateView renders a Django template and sends it to a browser
 
 def index(response):
     return HttpResponse("<h1>hi ozan<h1>")
@@ -12,3 +12,8 @@ def v1(response):
 
 class HomePageView(TemplateView): 
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['my_statement'] = 'nice to see you'
+        return context
