c23a086202678a0db6103ecea59c6427d50b8c21
Ozan <cetino19@itu.edu.tr>
2021-07-22 13:49:17 +0300

    Add museums app

--- everycheese/museums/models.py
+++ everycheese/museums/models.py
@@ -1,6 +1,22 @@
 from django.db import models
-from model_utils.models import TimeStampModel
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
 
 class Artifact(TimeStampedModel): 
-    name = models.Charfield("Name of Artifact", max_length = 200)
+    name = models.CharField("Name of Artifact", max_length = 200)
+    slug = AutoSlugField("Artifact Address",
+    unique=True, always_update=False, populate_from="name")
+    description = models.TextField("Description", blank = True)
+
+    class Firmness(models.TextChoices):
+        UNSPECIFIED = "unspecified", "Unspecified"
+        SOFT = "soft", "Soft"
+        SEMI_SOFT = "semi-soft", "Semi-Soft"
+        SEMI_HARD = "semi-hard", "Semi-Hard"
+        HARD = "hard", "Hard"
     
+    firmness = models.CharField("Firmness", max_length=20,
+    choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+
+    def __str__(self):
+        return self.name
